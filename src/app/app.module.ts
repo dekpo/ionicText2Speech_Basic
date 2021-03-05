12cf436fef9e0e3ef97a70e014d0d669a62a38bf
Dekpo Yologaza <dekpo@me.com>
2021-03-05 16:45:53 +0100

    Added all necessary in module, tabs and tab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    TextToSpeech,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
+const { Geolocation,Device,Network } = Plugins;
+
+let handler = Network.addListener('networkStatusChange', (status) => {
+  console.log("Network status changed", status);
+});
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +15,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private tts: TextToSpeech) {}
+
+  variable: string;
+
+
+  parleMaintenant(){
+
+    console.log('Texte lu:', this.variable );
+
+    this.tts.speak({
+      text: this.variable,
+      locale: 'fr-FR',
+      rate: 1
+    })
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+
+  }
+
+  async ionViewWillEnter(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+
+    const info = await Device.getInfo();
+    console.log('Device', info);
+
+    console.log('Ceci est un message dans la console');
+  }
 
 }
